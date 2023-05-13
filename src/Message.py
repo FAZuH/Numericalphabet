54c7fccf7d4eecc9b159d6d6276ddff36b456422
FAZuH <108622606+FAZuH@users.noreply.github.com>
2023-05-13 12:31:02 +0700

    Update Message.py

--- src/Message.py
+++ src/Message.py
@@ -1,7 +1,8 @@
-from colorama import Fore, Style
+from colorama import init, Fore, Style
 
 import Trainer
 
+init()
 def print_red(message):
     print(Fore.LIGHTRED_EX + message + Style.RESET_ALL)
 
@@ -11,7 +12,7 @@ def print_green(message):
 def print_blue(message):
     print(Fore.LIGHTBLUE_EX + message + Style.RESET_ALL)
 
-
+test_types = ['math', 'matchNumber', 'matchAlphabet']
 def user_input():
     global test_n, test_type
     while True:
@@ -19,11 +20,11 @@ def user_input():
         test_type = input()
         if str(test_type).lower() == 'exit':
             exit()
-        elif test_type not in ['math', 'matchNumber', 'matchAlphabet']:
+        elif test_type not in test_types:
             print_red("Invalid test type. Enter 'math', 'matchNumber' or 'matchAlphabet'")
             continue
         break
-
+    
     print_blue("\nEnter number of tests:")
     test_n = int(input())
 
@@ -32,4 +33,3 @@ def result():
     print_green(f'correct: {Trainer.correct}')
     print_green(f'incorrect: {Trainer.incorrect}')
     print_green(f'max combo: {Trainer.max_combo}')
-
